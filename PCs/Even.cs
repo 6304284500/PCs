0d02ffe96006c416acd8c7997d55993b4767c3a3
Kolavennu Sai Sreeram Kumar <sensational.sreeram999@gmail.com>
2023-03-14 05:11:10 +0530

    Programming constructs till 2 problems

---
+++ PCs/Even.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCs
+{
+    internal class UC2
+    {
+        public void two()
+        {
+            Console.WriteLine("Enter a Number to check weather Even or Odd");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num%2==0)
+            { Console.WriteLine("The given Number={0} is an Even number", num); }
+            else  
+            { Console.WriteLine("The given Number={0} is an Odd number", num); }
+
+            
+        }
+    }
+}
---
+++ PCs/Program.cs
@@ -0,0 +1,12 @@
+﻿using PCs;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        UC1 uC1 = new UC1();
+        uC1.one();
+        UC2 uC2 = new UC2();
+        uC2.two();
+    }
+}
